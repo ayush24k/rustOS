b6d4b7f5e5bc1c17ac47cc58c0d143e5e0d08b1d
Ayush <ayushanshu69@gmail.com>
2024-06-04 16:15:14 +0530

    rust kernel

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,22 @@ use core::panic::PanicInfo;
 // fn called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
+
+static OUTPUT: &[u8] = b"Ayush24k's Os Built in Rust";
 
 #[no_mangle] // to disable name mangling
 pub extern "C" fn _start() -> ! {
     //entry point, since linker looks for a fn
-    //named '_start' by default 
+    //named '_start' by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in OUTPUT.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xD;
+        }
+    }
     loop {}
 }
-
-
